a36d693b07029b639f78d75cbac38a362ddf8dcc
MicahJank <MicahJank@users.noreply.github.com>
2020-01-26 14:27:13 -0600

    added dependencies

--- crud/src/index.js
+++ crud/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+// Apollo is a bit like redux in that we need to use a provider
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import 'react-alice-carousel/lib/alice-carousel.css'
 import './carousel.css';
 import 'semantic-ui-css/semantic.min.css'
